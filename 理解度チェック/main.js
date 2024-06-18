0c0a5bfa3d9cf48dcc72ff8c9fcdff3c23b74247
TaichiKonnai <va21045@st.kitasato-u.ac.jp>
2024-06-18 19:14:58 +0900

    理解度チェック（繰り返し処理）

---
+++ 理解度チェック/main.js
@@ -0,0 +1,30 @@
+for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0){
+    console.log('Fizz!');
+    }else if (i % 5 === 0){
+    console.log('Buzz!');
+    }else if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz!');
+    }else {
+    console.log(i);
+    }
+}
+
+/*
+下が正しいやつ上はいろいろやってみたやつ
+上が何でダメなのか
+
+*/
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz!');
+    } else if (i % 3 === 0) {
+      console.log('Fizz!');
+    } else if (i % 5 === 0) {
+      console.log('Buzz!');
+    }else {
+      console.log(i);
+    }
+  }
+
