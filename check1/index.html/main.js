df548b8374aed71fe6b7c295f81293435fa2ff85
TaichiKonnai <va21045@st.kitasato-u.ac.jp>
2024-06-20 19:01:21 +0900

    JS_1章チェックリスト(修正)

--- check1/index.html/main.js
+++ check1/index.html/main.js
@@ -1,23 +1,24 @@
 //問１ isEven関数
 let numbers = [2, 5, 12, 13, 15, 18, 22];
-function isEven(num) {
-    if (num % 2 === 0) {
-        console.log(num + 'は偶数です');
+function isEven(){
+    for (let i = 0 ; i < numbers.length; i++){
+        if (numbers[i] % 2 === 0){
+            console.log(numbers[i] + 'は偶数です');
+        }
     }
 }
 
-    isEven(2);
-    isEven(5);
-    isEven(12);
-    isEven(13);
-    isEven(15);
-    isEven(18);
-    isEven(22);
-//ここ５時間かかりました...isEven()にconsole数字(今回num)を入れる
+isEven();
 
 //問２　getNumGas関数
-let car = {
-    gas: '〇〇',
-    num: '△△',
-};
-console.log("ガソリンは" + car.gas + "です。", "ナンバーは" + car.num + "です。");
+class car {
+    constructor(gas, num){
+        this.gas = '〇〇';
+        this.num = '△△';
+    }
+    getNumGas(){
+        console.log(`ガソリンは${this.gas}です。ナンバーは${this.num}です。`);
+    }
+}
+let 車 = new car('gas','num')
+車.getNumGas();
